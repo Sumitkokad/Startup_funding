cc954a819dc1a37f80c06ba4c8db20710cc8363d
Sumitkokad <148705098+Sumitkokad@users.noreply.github.com>
2026-05-21 21:18:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ if option=='Overall Analysis':
     with col2:
 
         with st.container(border=True):
-            st.header('Max Funding')
+            st.header('Maximum Funding')
             st.subheader(f'Cr {original['amount'].max()}')
 
 
